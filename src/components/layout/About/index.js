da69ff76a97a51e692ddc89e2b09285c4521f121
jamisu <a.pocentek@gmail.com>
2022-08-17 12:51:21 +0200

    duplicated code moved to single reusable component

--- src/components/layout/About/index.js
+++ src/components/layout/About/index.js
@@ -1,15 +1,12 @@
 import './index.scss'
-import BigIcon from '../../BigIcon'
 import { faReact, faJsSquare, faCss3, faHtml5, faSass, faPython, faJava, faAngular } from '@fortawesome/free-brands-svg-icons'
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import { useOutletContext } from "react-router-dom";
-import { gsap } from "gsap";
-import { Draggable } from "gsap/all";
-
+import IconHandler from '../../IconHandler';
 
 const About = () => {
-    
     const [dayMode] = useOutletContext();
+    const [selectedIndex, setSelectedIndex] = useState(8);
     const icons = [faReact, faJsSquare, faCss3, faHtml5, faSass, faPython, faJava, faAngular];
     const languageText = ['React:', 'JS:', 'CSS:', 'HTML:', 'SASS:', 'Python:', 'Java:', 'Angular:'];
     const aboutText = [
@@ -24,75 +21,6 @@ const About = () => {
 
     "These are My Skills:"];
 
-    const [selectedIndex, setSelectedIndex] = useState(8);
-    let draggables = useRef();
-
-    useEffect(() => {
-        gsap.registerPlugin(Draggable);
-        draggables.current = Draggable.create(".iconContainer", {
-                type: "x",
-                onPress: function() {
-                    if(window.innerWidth > 768) {
-                        draggables.current[0].disable()
-                    } else {
-                        draggables.current[0].enable()
-                    }
-                },
-                onRelease: function() {
-                    const endX = this.endX;
-                    const currentScreenOffset = [...this.target.children].map((el, i) => (el.getBoundingClientRect().x + el.getBoundingClientRect().width/2))
-                    const windowCenter = window.innerWidth/2;
-                    let indexClosestToCenter = 0;
-                    let lastClosestX = 10000;
-
-                    for(var i=0; i < currentScreenOffset.length; i++) {
-                        const currentItemDifference = Math.abs(currentScreenOffset[i] - windowCenter);
-                        if (currentItemDifference < lastClosestX) {
-                            lastClosestX = currentItemDifference;
-                            indexClosestToCenter = i;
-                        } else if (currentItemDifference >= lastClosestX) {
-                            break;
-                        }
-                        indexClosestToCenter = i;
-                    }
-                    setSelectedIndex(indexClosestToCenter);
-                    const correctionX = endX - currentScreenOffset[indexClosestToCenter] + windowCenter;
-
-                    gsap.to(".iconContainer", {
-                        x: correctionX,
-                        duration: .5,
-                        ease: "back"
-                    })
-                }
-            });
-        }
-    );
-
-    useEffect(() => {
-        window.addEventListener("resize", handleResize);
-        window.dispatchEvent(new Event('resize'));
-        return (e) => {
-            window.removeEventListener("resize", handleResize);
-        };
-    })
-    const handleResize = (e) => {
-        if (window.innerWidth > 768) {
-            draggables.current[0].target.style.left = "50%";
-            draggables.current[0].target.style.transform = "translate(-50%, -50%)";
-        }
-    }
-
-    const clickHandler = (e) => {
-        setSelectedIndex(e);
-    }
-    const onHoverHandler = (e) => {
-    }
-
-    const iconList = () => {
-        return icons.map((icon, i) => <BigIcon key={i} ease="bounceIn" selectedId={selectedIndex} index={i} 
-        onClickHandler={clickHandler} onHoverHandler={onHoverHandler} icon={icon}/>);
-    }
-
     return(
     <div className={'about ' + dayMode}>
             <div className={'aboutTextArea'}>
@@ -100,7 +28,7 @@ const About = () => {
                 <div>{aboutText[selectedIndex]}</div>
             </div>
             <div className={'iconContainer'} >
-                {iconList()} 
+                <IconHandler icons={icons} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
             </div>
     </div>)
 }
--- src/components/layout/Home/index.js
+++ src/components/layout/Home/index.js
@@ -1,85 +1,15 @@
 import './index.scss'
-import BigIcon from '../../BigIcon'
-import { faKitchenSet, faPersonHiking, faBiking, faPlateWheat, faCode, faMusic } from '@fortawesome/free-solid-svg-icons'
-import { useState, useEffect, useRef } from 'react';
+import { faKitchenSet, faPersonHiking, faBiking, faCode, faMusic } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
 import { useOutletContext } from "react-router-dom";
-import { gsap } from "gsap";
-import { Draggable } from "gsap/all";
+import IconHandler from '../../IconHandler';
 
 const Home = () => {
     const [dayMode] = useOutletContext();
+    const [selectedIndex, setSelectedIndex] = useState(5);
     const icons = [faPersonHiking, faBiking, faCode, faKitchenSet, faMusic];
     const homeText1 = ["I Love Mountain Hicking", "Cycling, cycling, ever cycling", "Coding is good for your brain",
      "Cooking is like an Alchemy", "Music production is the finest of the arts", "Welcome to the Chaotec Portfolio!"];
-
-    const [selectedIndex, setSelectedIndex] = useState(5);
-    let draggables = useRef();
-
-    useEffect(() => {
-        gsap.registerPlugin(Draggable);
-        draggables.current = Draggable.create(".iconContainer", {
-                type: "x",
-                onPress: function() {
-                    if(window.innerWidth > 768) {
-                        draggables.current[0].disable()
-                    } else {
-                        draggables.current[0].enable()
-                    }
-                },
-                onRelease: function() {
-                    const endX = this.endX;
-                    const currentScreenOffset = [...this.target.children].map((el, i) => (el.getBoundingClientRect().x + el.getBoundingClientRect().width/2))
-                    const windowCenter = window.innerWidth/2;
-                    let indexClosestToCenter = 0;
-                    let lastClosestX = 10000;
-
-                    for(var i=0; i < currentScreenOffset.length; i++) {
-                        const currentItemDifference = Math.abs(currentScreenOffset[i] - windowCenter);
-                        if (currentItemDifference < lastClosestX) {
-                            lastClosestX = currentItemDifference;
-                            indexClosestToCenter = i;
-                        } else if (currentItemDifference >= lastClosestX) {
-                            break;
-                        }
-                        indexClosestToCenter = i;
-                    }
-                    setSelectedIndex(indexClosestToCenter);
-                    const correctionX = endX - currentScreenOffset[indexClosestToCenter] + windowCenter;
-
-                    gsap.to(".iconContainer", {
-                        x: correctionX,
-                        duration: .5,
-                        ease: "back"
-                    })
-                }
-            });
-        }
-    );
-
-    useEffect(() => {
-        window.addEventListener("resize", handleResize);
-        window.dispatchEvent(new Event('resize'));
-        return (e) => {
-            window.removeEventListener("resize", handleResize);
-        };
-    })
-    const handleResize = (e) => {
-        if (window.innerWidth > 768) {
-            draggables.current[0].target.style.left = "50%";
-            draggables.current[0].target.style.transform = "translate(-50%, -50%)";
-        }
-    }
-
-    const clickHandler = (e) => {
-        setSelectedIndex(e);
-    }
-    const onHoverHandler = (e) => {
-    }
-
-    const iconList = () => {
-        return icons.map((icon, i) => <BigIcon key={i} ease="bounceIn" selectedId={selectedIndex} index={i} 
-        onClickHandler={clickHandler} onHoverHandler={onHoverHandler} icon={icon}/>);
-    }
 
     return(
         <div className={"home " + dayMode}>
@@ -96,11 +26,11 @@ const Home = () => {
             </div>
             
             <div className='iconContainer' >
-                {iconList()}
+                <IconHandler icons={icons} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
             </div>
 
             <div className='homeTextArea'>
-                <h>{homeText1[selectedIndex]}</h>
+                <span>{homeText1[selectedIndex]}</span>
             </div>
         </div>
         )
