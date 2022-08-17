db0fa018673c2bc11af08b84335b5a2cd4c510d3
jamisu <a.pocentek@gmail.com>
2022-08-17 12:00:09 +0200

    about and home cleanup

--- src/components/layout/About/index.js
+++ src/components/layout/About/index.js
@@ -25,40 +25,20 @@ const About = () => {
     "These are My Skills:"];
 
     const [selectedIndex, setSelectedIndex] = useState(8);
-    const [mobile, setMobile] = useState(false);
     let draggables = useRef();
     let itemListArray = [];
-
-    const onHoverHandler = (e) => {
-        // setSelectedIndex(e);
-    }
-
-    const handleResize = (e) => {
-        if (window.innerWidth <= 768 && !mobile) {
-            // const initIndex = selectedIndex < 8 ? selectedIndex : 0;
-            // document.getElementsByClassName('iconContainer')[0].style.transform = "translate(0, -50%)"
-            // const bigs = document.querySelectorAll('.bigIcon');
-            // const offset = window.innerWidth/2 - bigs[initIndex].offsetLeft - bigs[initIndex].offsetWidth/2;
-            // document.getElementsByClassName('iconContainer')[0].style.left = offset + 'px';
-            // console.log('set mobile on resize', initIndex);
-            draggables.current[0].enable()
-            setMobile(true);
-
-        } else if (window.innerWidth > 768 && mobile) {
-            draggables.current[0].target.style.left = "50%";
-            draggables.current[0].target.style.transform = "translate(-50%, -50%)";
-            console.log('unset mobile', draggables.current[0]);
-            draggables.current[0].disable()
-            setMobile(false);
-        }
-    }
 
     useEffect(() => {
         gsap.registerPlugin(Draggable);
         draggables.current = Draggable.create(".iconContainer", {
                 type: "x",
                 onPress: function() {
-                    if(!mobile) {draggables.current[0].disable()}
+                    if(window.innerWidth > 768) {
+                        draggables.current[0].disable()
+                    } else {
+                        draggables.current[0].enable()
+                    }
+                    // if(!mobile) {draggables.current[0].disable()}
                 },
                 onRelease: function() {
                     const endX = this.endX;
@@ -91,22 +71,24 @@ const About = () => {
     );
 
     useEffect(() => {
-        console.log('about register resize, mobile:', mobile);
         window.addEventListener("resize", handleResize);
         window.dispatchEvent(new Event('resize'));
         return (e) => {
             window.removeEventListener("resize", handleResize);
         };
     })
+    const handleResize = (e) => {
+        if (window.innerWidth > 768) {
+            draggables.current[0].target.style.left = "50%";
+            draggables.current[0].target.style.transform = "translate(-50%, -50%)";
+        }
+    }
 
     const clickHandler = (e) => {
-        if (mobile) {
-            // const bigs = document.querySelectorAll('.bigIcon');
-            // const offset = window.innerWidth/2 - bigs[e].offsetLeft - bigs[e].offsetWidth/2;
-
-            // document.getElementsByClassName('iconContainer')[0].style.left = offset + 'px';
-        }
         setSelectedIndex(e);
+    }
+    const onHoverHandler = (e) => {
+        // setSelectedIndex(e);
     }
 
     const iconList = () => {
--- src/components/layout/Home/index.js
+++ src/components/layout/Home/index.js
@@ -13,40 +13,20 @@ const Home = () => {
      "Cooking is like an Alchemy", "Music production is the finest of the arts", "Welcome to the Chaotec Portfolio!"];
 
     const [selectedIndex, setSelectedIndex] = useState(5);
-    const [mobile, setMobile] = useState(false);
     let draggables = useRef();
     let itemListArray = [];
-
-    const onHoverHandler = (e) => {
-        // setSelectedIndex(e);
-    }
-
-    const handleResize = (e) => {
-        if (window.innerWidth <= 768 && !mobile) {
-            // const initIndex = selectedIndex < 8 ? selectedIndex : 0;
-            // document.getElementsByClassName('iconContainer')[0].style.transform = "translate(0, -50%)"
-            // const bigs = document.querySelectorAll('.bigIcon');
-            // const offset = window.innerWidth/2 - bigs[initIndex].offsetLeft - bigs[initIndex].offsetWidth/2;
-            // document.getElementsByClassName('iconContainer')[0].style.left = offset + 'px';
-            // console.log('set mobile on resize', initIndex);
-            draggables.current[0].enable()
-            setMobile(true);
-
-        } else if (window.innerWidth > 768 && mobile) {
-            draggables.current[0].target.style.left = "50%";
-            draggables.current[0].target.style.transform = "translate(-50%, -50%)";
-            console.log('unset mobile', draggables.current[0]);
-            draggables.current[0].disable()
-            setMobile(false);
-        }
-    }
 
     useEffect(() => {
         gsap.registerPlugin(Draggable);
         draggables.current = Draggable.create(".iconContainer", {
                 type: "x",
                 onPress: function() {
-                    if(!mobile) {draggables.current[0].disable()}
+                    if(window.innerWidth > 768) {
+                        draggables.current[0].disable()
+                    } else {
+                        draggables.current[0].enable()
+                    }
+                    // if(!mobile) {draggables.current[0].disable()}
                 },
                 onRelease: function() {
                     const endX = this.endX;
@@ -79,23 +59,24 @@ const Home = () => {
     );
 
     useEffect(() => {
-        console.log('about register resize, mobile:', mobile);
         window.addEventListener("resize", handleResize);
         window.dispatchEvent(new Event('resize'));
         return (e) => {
             window.removeEventListener("resize", handleResize);
         };
     })
+    const handleResize = (e) => {
+        if (window.innerWidth > 768) {
+            draggables.current[0].target.style.left = "50%";
+            draggables.current[0].target.style.transform = "translate(-50%, -50%)";
+        }
+    }
 
     const clickHandler = (e) => {
-        if (mobile) {
-            // const bigs = document.querySelectorAll('.bigIcon');
-            // const offset = window.innerWidth/2 - bigs[e].offsetLeft - bigs[e].offsetWidth/2;
-
-            // document.getElementsByClassName('iconContainer')[0].style.left = offset + 'px';
-        }
-        
         setSelectedIndex(e);
+    }
+    const onHoverHandler = (e) => {
+        // setSelectedIndex(e);
     }
 
     const iconList = () => {
@@ -124,7 +105,7 @@ const Home = () => {
             </div>
 
             <div className='homeTextArea'>
-                <div>{homeText1[selectedIndex]}</div>
+                <h>{homeText1[selectedIndex]}</h>
             </div>
         </div>
         )
