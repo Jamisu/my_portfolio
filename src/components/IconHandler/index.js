d8d5e2536dbe477226dc7fcd9dda4e049d607145
jamisu <a.pocentek@gmail.com>
2023-11-21 15:36:31 +0100

    some error fixes

--- src/components/IconHandler/index.js
+++ src/components/IconHandler/index.js
@@ -9,119 +9,6 @@ const IconHandler = (params) => {
     const containerRef = useRef();
     const iconsArrRef = useRef();
 
-    useEffect(() => {
-        const container = containerRef.current = document.getElementsByClassName('iconContainer')[0]
-        let mouseStart;
-        let containerLeft;
-        let setIntervalID;
-        let mouseX;
-        let resizeId;
-        let bounds = {};
-        
-        const dragHandler = e => {
-            setContainerX();
-        }
-
-        const setBoundsAndStartPos = e => {
-            bounds = {'left' : container.clientWidth/2 + window.innerWidth/2,
-                    'right' : window.innerWidth/2 - container.clientWidth/2};
-
-            mouseStart = mouseX = e.clientX || e.changedTouches[0].clientX;
-            if (container.style.left) {
-                containerLeft = parseInt(container.style.left)
-            } else {
-                containerLeft = window.innerWidth/2;
-            }
-        }
-
-        const setContainerX = e => {
-            const newLeft = containerLeft - mouseStart + mouseX;
-             if (newLeft < bounds.left && newLeft > bounds.right) {
-                setContainerLeft(newLeft);
-             }
-        }
-
-        const setContainerLeft = nl => {
-            container.style.left = nl  + 'px';
-            console.log('container.style.left', container.style.left)
-        }
-        
-        /// DESKTOP MOUSE ///
-        const handleDocumentMouseDown = e => {
-            setBoundsAndStartPos(e);   
-            window.addEventListener('mouseup', handleDocumentMouseUp);
-            if(window.innerWidth < container.clientWidth) {
-                window.onmousemove = function(e) {
-                    mouseX = e.x
-                }  
-                setIntervalID = setInterval(dragHandler, 25)
-            }
-        };
-        const handleDocumentMouseUp = e => {
-            window.removeEventListener('mouseup', handleDocumentMouseUp);
-            window.onmousemove = null;
-            clearInterval(setIntervalID)
-            if(!isContentWide()) {
-                return;
-            }
-            if(Math.abs(mouseStart - e.clientX) > 3) {
-                wasDragged.current = true;
-                return;
-            } else {
-                wasDragged.current = false;
-            }
-        }
-
-        /// MOBILE TOUCH MOUSE ///
-        const handleTouchMove = e => {
-            mouseX = e.changedTouches[0].clientX;
-        }
-        const handleTouchStart = e => {
-            setBoundsAndStartPos(e);
-            setIntervalID = setInterval(dragHandler, 25)
-        }
-        const handleTouchEnd = e => {
-            clearInterval(setIntervalID)
-        }
-
-        const handleResize = e => {
-            clearTimeout(resizeId);
-            resizeId = setTimeout(onResizeStop, 100);
-        }
-        const onResizeStop = e => {
-                if(isContentWide()) {
-                    // center to selected || center to client center
-                    setContainerLeft(container.clientWidth/2);
-                    snapToSelected();
-                // Center container
-                } else {
-                    setContainerLeft(window.innerWidth/2);
-                }
-        }
-        onResizeStop(null);
-
-        container.addEventListener('mousedown', handleDocumentMouseDown);
-        window.addEventListener('resize', handleResize);
-
-        container.addEventListener('touchmove', handleTouchMove);
-        container.addEventListener('touchstart', handleTouchStart);
-        container.addEventListener('touchend', handleTouchEnd);
-
-        return () => {
-            window.removeEventListener('mouseup', handleDocumentMouseUp);
-            container.removeEventListener('mousedown', handleDocumentMouseDown);
-            window.removeEventListener('resize', handleResize);
-
-            container.removeEventListener('touchmove', handleTouchMove);
-            container.removeEventListener('touchstart', handleTouchStart);
-            container.removeEventListener('touchend', handleTouchEnd);
-
-            window.onmousemove = null;
-            clearInterval(setIntervalID);
-            wasDragged.current = false;
-        };
-    });
-
     const isContentWide = e => {
         return containerRef.current.clientWidth > window.innerWidth;
     }
@@ -134,9 +21,13 @@ const IconHandler = (params) => {
     }
 
     const snapToSelected = (si = selectedIndex) => {
+        console.log('si ' + si);
+
         if(si === iconsArrRef.current.length) {
             return;
         }
+        
+        
         containerRef.current.style.left = 
         window.innerWidth/2 + containerRef.current.clientWidth/2 - (iconsArrRef.current[si].offsetLeft +
             iconsArrRef.current[si].getBoundingClientRect().width/2) + 'px';
@@ -147,6 +38,123 @@ const IconHandler = (params) => {
             onClickHandler={clickHandler} onHoverHandler={e=>e} icon={icon}/>)
         return elemArr;
     }
+
+    useEffect(() => {
+        const container = containerRef.current = document.getElementsByClassName('iconContainer')[0];
+        let mouseStart;
+        let containerLeft;
+        let setIntervalID;
+        let mouseX;
+        let resizeId;
+        let bounds = {};
+    
+        const dragHandler = (e) => {
+            setContainerX();
+        };
+    
+        const setBoundsAndStartPos = (e) => {
+            bounds = {
+                'left': container.clientWidth / 2 + window.innerWidth / 2,
+                'right': window.innerWidth / 2 - container.clientWidth / 2
+            };
+    
+            mouseStart = mouseX = e.clientX || e.changedTouches[0].clientX;
+            if (container.style.left) {
+                containerLeft = parseInt(container.style.left);
+            } else {
+                containerLeft = window.innerWidth / 2;
+            }
+        };
+    
+        const setContainerX = (e) => {
+            const newLeft = containerLeft - mouseStart + mouseX;
+            if (newLeft < bounds.left && newLeft > bounds.right) {
+                setContainerLeft(newLeft);
+            }
+        };
+    
+        const setContainerLeft = (nl) => {
+            container.style.left = nl + 'px';
+            console.log('container.style.left', container.style.left);
+        };
+    
+        // DESKTOP MOUSE //
+        const handleDocumentMouseDown = (e) => {
+            setBoundsAndStartPos(e);
+            window.addEventListener('mouseup', handleDocumentMouseUp);
+            if (window.innerWidth < container.clientWidth) {
+                window.onmousemove = function (e) {
+                    mouseX = e.x;
+                };
+                setIntervalID = setInterval(dragHandler, 25);
+            }
+        };
+        const handleDocumentMouseUp = (e) => {
+            window.removeEventListener('mouseup', handleDocumentMouseUp);
+            window.onmousemove = null;
+            clearInterval(setIntervalID);
+            if (!isContentWide()) {
+                return;
+            }
+            if (Math.abs(mouseStart - e.clientX) > 3) {
+                wasDragged.current = true;
+                return;
+            } else {
+                wasDragged.current = false;
+            }
+        };
+    
+        // MOBILE TOUCH MOUSE //
+        const handleTouchMove = (e) => {
+            mouseX = e.changedTouches[0].clientX;
+        };
+        const handleTouchStart = (e) => {
+            setBoundsAndStartPos(e);
+            setIntervalID = setInterval(dragHandler, 25);
+        };
+        const handleTouchEnd = (e) => {
+            clearInterval(setIntervalID);
+        };
+    
+        const handleResize = (e) => {
+            clearTimeout(resizeId);
+            resizeId = setTimeout(onResizeStop, 100);
+        };
+        const onResizeStop = (e) => {
+            if (isContentWide()) {
+                // center to selected || center to client center
+                setContainerLeft(container.clientWidth / 2);
+                snapToSelected();
+                // Center container
+            } else {
+                setContainerLeft(window.innerWidth / 2);
+            }
+        };
+        onResizeStop(null);
+    
+        container.addEventListener('mousedown', handleDocumentMouseDown);
+        window.addEventListener('resize', handleResize);
+    
+        container.addEventListener('touchmove', handleTouchMove);
+        container.addEventListener('touchstart', handleTouchStart);
+        container.addEventListener('touchend', handleTouchEnd);
+    
+        return () => {
+            window.removeEventListener('mouseup', handleDocumentMouseUp);
+            container.removeEventListener('mousedown', handleDocumentMouseDown);
+            window.removeEventListener('resize', handleResize);
+    
+            container.removeEventListener('touchmove', handleTouchMove);
+            container.removeEventListener('touchstart', handleTouchStart);
+            container.removeEventListener('touchend', handleTouchEnd);
+    
+            window.onmousemove = null;
+            clearInterval(setIntervalID);
+            wasDragged.current = false;
+        };
+    });
+
+    
 
     return (
         iconList()
